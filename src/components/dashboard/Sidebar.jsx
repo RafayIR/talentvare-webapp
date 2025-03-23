eb2267c4ad132b338274dd27ce0235523528faac
Rafay Iqbal <43880610+RafayIR@users.noreply.github.com>
2025-03-23 23:53:25 +0500

    change in import

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,7 +1,6 @@
 'use client'
 import { useState } from "react";
 import { useSidebarStore, usePopoverStore } from "@/lib/stores/sidebarStore";
-import Image from "next/image";
 import ProfileWrapper from "./ProfileWrapper";
 
 
